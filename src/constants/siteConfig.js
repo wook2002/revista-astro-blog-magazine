18fc97621443381efcdef7183c5f36fabc79200c
JaeUg Lee <87742894+wook2002@users.noreply.github.com>
2026-04-14 16:38:46 +0900

    Create siteConfig.js

---
+++ src/constants/siteConfig.js
@@ -0,0 +1,12 @@
+export const SITE_INFO = {
+  // 로고 옆 사이트 이름 (현재 '네브바1' 부분을 대체함)
+  title: {
+    main: "Sim",
+    sub: "log",
+  },
+  // 오른쪽 버튼 글자와 링크
+  navButton: {
+    text: "가족 소개",
+    url: "/support-us",
+  },
+};
