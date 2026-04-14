621b0928fe0d44869d376baa1a6062036e7fafa3
JaeUg Lee <87742894+wook2002@users.noreply.github.com>
2026-04-14 16:44:49 +0900

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { FaBars } from 'react-icons/fa'
 import Links from '../constants/links'
+// 1. 방금 만든 설정 파일 불러오기
+import { SITE_INFO } from '../constants/siteConfig' 
 import heart from '../assets/heart.png'
 import logo from '../assets/logo.png'
 import '../css/navbar.css'
@@ -12,10 +14,12 @@ const Navbar = ({ toggle }) => {
         <div className="nav-center">
           <div className="nav-header">
             <a href="/" className="nav-logo not-blue">
-              <img src={logo.src} alt="Revista Logo" width="100" height="100" />
+              {/* 로고 alt 텍스트에 사이트 이름 적용 */}
+              <img src={logo.src} alt={`${SITE_INFO.title.main}${SITE_INFO.title.sub} 로고`} width="100" height="100" />
               <div className="logo-text">
-                <span className="nav-word-open">네브</span>
-                <span className="nav-word-ed">바1</span>
+                {/* 2. 하드코딩된 이름을 설정값으로 교체 */}
+                <span className="nav-word-open">{SITE_INFO.title.main}</span>
+                <span className="nav-word-ed">{SITE_INFO.title.sub}</span>
               </div>
             </a>
 
@@ -29,11 +33,13 @@ const Navbar = ({ toggle }) => {
           </div>
 
           <Links styleClass="nav-links" />
+          
+          {/* 3. 오른쪽 버튼의 링크와 글자를 설정값으로 교체 */}
           <a
             className="support-us-link not-blue"
             target="_blank"
             rel="noreferrer noopener"
-            href="/support-us"
+            href={SITE_INFO.navButton.url}
           >
             <img
               src={heart.src}
@@ -42,7 +48,7 @@ const Navbar = ({ toggle }) => {
               width="19"
               height="19"
             />
-            Support Us
+            {SITE_INFO.navButton.text}
           </a>
         </div>
       </nav>
